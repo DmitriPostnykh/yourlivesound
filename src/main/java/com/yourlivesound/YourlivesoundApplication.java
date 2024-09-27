58e35126db059a6cffddf98453bc4294453090a6
DmitriPostnykh <javadeveloper7377@gmail.com>
2024-09-27 03:49:52 -0700

    first commit

---
+++ src/main/java/com/yourlivesound/YourlivesoundApplication.java
@@ -0,0 +1,13 @@
+package com.yourlivesound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourlivesoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourlivesoundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yourlivesound/YourlivesoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourlivesound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourlivesoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
